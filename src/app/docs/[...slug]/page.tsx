e59de58cf4e879ba60b4f9e3a2f523c88dbe02e7
DopplerXD <1509209607@qq.com>
2026-04-19 01:05:06 +0800

    Update page.tsx

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -1,5 +1,25 @@
 import { redirect } from "next/navigation";
 
+const staticDocSlugs = [
+  "index",
+  "algorithm/index",
+  "tech/index",
+  "dev/index",
+  "dev/tree-command",
+  "leetcode/index",
+  "resource/index",
+  "resource/good-web",
+  "notes/clean-code",
+] as const;
+
+export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return staticDocSlugs.map((slug) => ({
+    slug: slug.split("/"),
+  }));
+}
+
 export default function DocSlugPage() {
   redirect("/");
 }
